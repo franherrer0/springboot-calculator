fb6a03e2035e72759b8563419b784c362f075701
Fran <fran.herrero@gmail.com>
2020-04-30 16:41:18 +0200

    Added logic to controller

--- src/main/java/es/fran/herrero/calculator/controller/v1/CalcController.java
+++ src/main/java/es/fran/herrero/calculator/controller/v1/CalcController.java
@@ -1,5 +1,6 @@
 package es.fran.herrero.calculator.controller.v1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,24 +11,37 @@ import org.springframework.web.bind.annotation.RestController;
 import es.fran.herrero.calculator.exception.CalcException;
 import es.fran.herrero.calculator.model.v1.CalcInputData;
 import es.fran.herrero.calculator.model.v1.CalcOutputData;
+import es.fran.herrero.calculator.service.v1.CalcService;
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 @RestController
 @RequestMapping("/v1")
 public class CalcController {
 
+	    @Autowired private CalcService calcService;
+
 	    @GetMapping("/calc/add/{op1}/{op2}")
 	    CalcOutputData add(@PathVariable double op1, @PathVariable double op2) {
-	    	return null;
+	    	CalcInputData inputData = new CalcInputData(op1,op2);
+	    	inputData.setOperator("+");
+	    	log.info("add:: " + inputData);
+	    	return new CalcOutputData(inputData.toString(), calcService.add(inputData));
 	    }
 
 	    @GetMapping("/calc/subtract/{op1}/{op2}")
 	    CalcOutputData subtract(@PathVariable double op1, @PathVariable double op2) {
-	    	return null;
+	    	CalcInputData inputData = new CalcInputData(op1,op2);
+	    	inputData.setOperator("-");
+	    	log.info("subtract:: " + inputData);
+	    	return new CalcOutputData(inputData.toString(), calcService.subtract(inputData));
 	    }
 	    
 	    @RequestMapping(value="/calc" , method = RequestMethod.POST)
 	    public CalcOutputData calc(@RequestBody CalcInputData input)
 	    throws CalcException {
-	    	return null;
+	    	log.info("calc:: " + input);
+	        return new CalcOutputData(input.toString(), calcService.calculate(input));
 	    }
 }
