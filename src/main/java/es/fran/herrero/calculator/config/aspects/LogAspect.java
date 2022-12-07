d856dfa79c7d94f26b7069f0f2784e1ba6a0034d
Fran <fran.herrero@gmail.com>
2022-12-07 12:07:59 +0100

    Remove log reference not needed

--- src/main/java/es/fran/herrero/calculator/config/aspects/LogAspect.java
+++ src/main/java/es/fran/herrero/calculator/config/aspects/LogAspect.java
@@ -5,7 +5,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
-import jdk.internal.org.jline.utils.Log;
 import lombok.extern.slf4j.Slf4j;
 
 @Aspect
